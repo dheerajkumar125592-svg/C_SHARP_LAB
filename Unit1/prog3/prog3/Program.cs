0fdc1f6aface0c3594b213544476417fbc1e14a8
Dheera kumar <dheerajkumar.125592@marwadiuniversity.ac.in>
2026-04-11 21:38:21 +0530

    Add files via upload

---
+++ Unit1/prog3/prog3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double principal, rate, time, simpleInterest;
+
+            Console.Write("Enter Principal Amount: ");
+            principal = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Rate of Interest (%): ");
+            rate = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Time (in years): ");
+            time = Convert.ToDouble(Console.ReadLine());
+
+            simpleInterest = (principal * rate * time) / 100;
+
+            Console.WriteLine("Simple Interest = " + simpleInterest);
+        }
+    }
+}
+   
+
